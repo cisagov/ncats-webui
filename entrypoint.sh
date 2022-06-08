d7ff6bb99b8201de7109d60d6ee3173fab45051a
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-06-08 03:15:19 -0400

    Update the entrypoint script
    Reformat the script with the shfmt hook and add some of the boilerplate
    we use in shellscripts.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
 export PATH=./node_modules/.bin:$PATH
 
 case "$1" in
 
-build) echo "Building to dist/ ..."
+  build)
+    echo "Building to dist/ ..."
     make build
     mkdir -p /usr/src/build
     cp -r dist /usr/src/build
     ;;
-*) echo "Starting up development server..."
-   make build-dev
-   ng serve --host 0.0.0.0 --port 4200 &  PIDSERVE=$!
-   ng test --port 9876 &  PIDTEST=$!
-   wait $PIDSERVE
-   wait $PIDTEST
-   ;;
+  *)
+    echo "Starting up development server..."
+    make build-dev
+    ng serve --host 0.0.0.0 --port 4200 &
+    PIDSERVE=$!
+    ng test --port 9876 &
+    PIDTEST=$!
+    wait $PIDSERVE
+    wait $PIDTEST
+    ;;
 esac
